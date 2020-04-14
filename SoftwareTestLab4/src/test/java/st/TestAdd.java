6f57d1431052579b82c183646701d866e62b4e70
GrandyJames <grandyjames@163.com>
2020-04-14 18:00:07 +0800

    The Lab 4 code

---
+++ SoftwareTestLab4/src/test/java/st/TestAdd.java
@@ -0,0 +1,15 @@
+package st;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Liu
+ */
+public class TestAdd {
+    @Test
+    public void verify() {
+        assertEquals(3,Add.Add(1,2));
+    }
+}
